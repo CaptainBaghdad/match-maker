98c47c82803d3ea3181621395db7ace8f8850bb9
me <juniorjr083@gmail.com>
2019-10-18 12:49:19 -0400

    added bio component

--- src/components/profile/profileImages.js
+++ src/components/profile/profileImages.js
@@ -1,47 +1,201 @@
-import React from 'react';
-import { FormControl } from '@material-ui/core';
-import Grid from '@material-ui/core/Grid';
-import Button from '@material-ui/core/Button';
-import {useReducer, useState,useEffect} from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import userReducer from '../../reducers/userReducer';
+import React, {useReducer, useEffect, useState} from 'react';
 import Avatar from '@material-ui/core/Avatar';
-import AddCircleIcon from '@material-ui/icons/AddCircle';
-import ButtonBase from '@material-ui/core/ButtonBase';
-import AddIcon from '@material-ui/icons/Add';
-import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+import clsx from 'clsx';
+import Card from '@material-ui/core/Card';
+import CardHeader from '@material-ui/core/CardHeader';
+import CardMedia from '@material-ui/core/CardMedia';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
+import Collapse from '@material-ui/core/Collapse';
+//import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
-import Tooltip from '@material-ui/core/Tooltip';
-import {Link} from 'react-router-dom';
-import Modal from '@material-ui/core/Modal';
+import Typography from '@material-ui/core/Typography';
+import { red } from '@material-ui/core/colors';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import ShareIcon from '@material-ui/icons/Share';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+import MoreVertIcon from '@material-ui/icons/MoreVert';
+import userReducer from '../../reducers/userReducer';
 import initState from '../../globalState';
-import axios  from 'axios';
+import AddBioComponent from './bioComponent';
 
-
-
-let ImageHolder = (props) =>{
-    //console.log(`COMPOENENT DID MOUNT`)
-   
-    
-    
-    
-    
+const useStyles = makeStyles(theme => ({
+    jumbo:{
+        height: 350,
+        width: '66%',
+        background: 'grey',
+        position: 'relative',
+        top: 0,
+        left: 0
+    },
+     bigAvatar: {
+       margin: 10,
+       width: 75,
+       height: 100,
+       position: 'relative',
+       top: 10,
+       left: 30
+     },
+     circle: {
+        
+         top: 70
+        
+     },
+     paper: {
+         position: 'absolute',
+         width: 400,
+         backgroundColor: 'white',
+         border: '2px solid #000',
+         left: 35,
+         height: 200
+         
+       },
+       submit: {
+           width: 30,
+           height: 30
  
-   
+       },
+       jumboImage: {
+         height: 350,
+         width: '66%'
+       },
+       card: {
+        maxWidth: 345,
+        position: 'relative',
+        top: 0
+      },
+      media: {
+        height: 0,
+        paddingTop: '56.25%', // 16:9
+      },
+      expand: {
+        transform: 'rotate(0deg)',
+        marginLeft: 'auto',
+        transition: theme.transitions.create('transform', {
+          duration: theme.transitions.duration.shortest,
+        }),
+      },
+      expandOpen: {
+        transform: 'rotate(180deg)',
+      },
+   }));
+
+
+
+let ProfileImageComponent = (props) =>{
+    let [globalObject, dispatch] = useReducer(userReducer, initState);
+    const classes = useStyles();
+    let [hasProfile, setProfile] = useState(false);
+    const [expanded, setExpanded] = React.useState(false);
     
-   
-     
+    const handleExpandClick = () => {
+        setExpanded(!expanded);
+      };
+    
+      useEffect(() =>{
+       // console.log('USE EFFECT FIRED')
+        let name = localStorage.getItem('name');
+        let findName = new FormData();
+        findName.append('name', name)
+        fetch('http://localhost:4677/get-user', {
+            method: "POST",
+            body: findName      
+        })
+        .then(res => res.json())
+        .then(data =>{
+           dispatch({type: 'LOAD_CARD', payload: data})
+           
+          
+             
+        })
+       
+       
+       
+    },[])
+    
     
     return (
-        
-        <div>This is the image holder</div>
-
-            )
-
-
+        <div>
+             <Card className={classes.card}>
+          <CardHeader
+            avatar={
+                <Avatar src={`http://localhost:4677/images/${globalObject.profilePic}`}
+                alt="there is a problem"
+                className={classes.bigAvatar}
+                />
+            }
+            action={
+              <IconButton aria-label="settings">
+                <MoreVertIcon />
+              </IconButton>
+            }
+            title={globalObject.name}
+            subheader={globalObject.region}
+          />
+          <CardMedia
+            className={classes.media}
+            image={globalObject.backgroundPic == '' ? `http://localhost:4677/images/backgroundImages/defaultBackgroundProfile.jpg` : `http://localhost:4677/images/backgroundImages/${globalObject.backgroundPic}`}
+            title="Paella dish"
+          />
+          <CardContent>
+            <Typography variant="body2" color="textSecondary" component="p">
+            {globalObject.bio == null ? (<AddBioComponent />) : globalObject.bio}
+            </Typography>
+          </CardContent>
+          <CardActions disableSpacing>
+            <IconButton aria-label="add to favorites">
+              <FavoriteIcon />
+            </IconButton>
+            <IconButton aria-label="share">
+              <ShareIcon />
+            </IconButton>
+            <IconButton
+              className={clsx(classes.expand, {
+                [classes.expandOpen]: expanded,
+              })}
+              onClick={handleExpandClick}
+              aria-expanded={expanded}
+              aria-label="show more"
+            >
+              <ExpandMoreIcon />
+            </IconButton>
+          </CardActions>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            <CardContent>
+              <Typography paragraph>Method:</Typography>
+              <Typography paragraph>
+                Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
+                minutes.
+              </Typography>
+              <Typography paragraph>
+                Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
+                heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
+                browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving chicken
+                and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion, salt and
+                pepper, and cook, stirring often until thickened and fragrant, about 10 minutes. Add
+                saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
+              </Typography>
+              <Typography paragraph>
+                Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
+                without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat to
+                medium-low, add reserved shrimp and mussels, tucking them down into the rice, and cook
+                again without stirring, until mussels have opened and rice is just tender, 5 to 7
+                minutes more. (Discard any mussels that don’t open.)
+              </Typography>
+              <Typography>
+                Set aside off of the heat to let rest for 10 minutes, and then serve.
+              </Typography>
+            </CardContent>
+          </Collapse>
+        </Card>
+       
+    
+        </div>
+    )
     
 
 
 }
 
-export default ImageHolder;
+export default ProfileImageComponent;
